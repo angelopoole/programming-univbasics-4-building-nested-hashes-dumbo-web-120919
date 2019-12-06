3781a65ce6737832e536046b82bc21023ac053fe
Angelo Poole <angelo.poole@outlook.com>
2019-12-06 00:42:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,10 +12,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	monopoly = {
-		railroads: {
-			:peices => {}
-		}
-	}
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars: {}
+    }
+  }
 end
 
 def monopoly_with_third_tier
